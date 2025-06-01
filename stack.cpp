242feff14d08f1763d749a65f7a6e39129544850
ivanadinata78 <ivan.adienata7@gmail.com>
2025-06-01 17:32:56 +0700

    deklarasi

---
+++ stack.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stack>
+#define MAXSTACK 5
+using namespace std;
+
+typedef int ItemType;
+
+typedef struct{
+    ItemType Item[MAXSTACK];
+    int Count;
+}stack;
+
+
+
