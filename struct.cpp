718b190f4b8b3cc272b1e4f5b60cdc6b57d0773e
ivanadinata78 <ivan.adienata7@gmail.com>
2025-06-01 16:57:56 +0700

    Penambahan pointer

--- struct.cpp
+++ struct.cpp
@@ -9,15 +9,21 @@ int main(){
         float ipk;
     };
 
-mahasiswa ivan;
-ivan.nim="C030324123";
-ivan.nama="Ivan Adinata";
-ivan.alamat="JL. Sultan Adam, Komplek Mahligai RT.12 RW.003";
-ivan.ipk=3.7;
+        mahasiswa ivan;
+        ivan.nim="C030324123";
+        ivan.nama="Ivan Adinata";
+        ivan.alamat="JL. Sultan Adam, Komplek Mahligai RT.12 RW.003";
+        ivan.ipk=3.7;
+
+        string *nim=&ivan.nim;
+        string *nama=&ivan.nama;
+        string *alamtat=&ivan.alamat;
+        float *ipk=&ivan.ipk;
+
 
 system("cls");
 cout << ivan.nim<<"\n";
-cout << ivan.nama<<"\n";
-cout << ivan.alamat<<"\n";
-cout << ivan.ipk<<"\n";
+        cout << ivan.nama<<"\n";
+        cout << ivan.alamat<<"\n";
+        cout << ivan.ipk<<"\n";
 }
